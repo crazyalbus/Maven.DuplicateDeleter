9b801e81a461c7b771310804a03c7f8374d5a424
Kate Bayard <katherinebayard@zipcoders-MBP-8.lan>
2019-03-04 06:56:50 -0500

    completed

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,8 +1,78 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+//    Integer[] intArray;
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+    }
+
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+
+        Integer[] results = new Integer[array.length];
+        int toReturn = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(countDuplicates(array[i], array) < maxNumberOfDuplications) {
+                results[i] = array[i];
+                toReturn++;
+            }
+        }
+
+        Integer[] trimmedArray = trimArray(results, toReturn);
+
+        return trimmedArray;
+    }
+
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+
+        Integer[] exactResults = new Integer[array.length];
+        int toReturn = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(countDuplicates(array[i], array) != exactNumberOfDuplications) {
+                exactResults[i] = array[i];
+                toReturn++;
+            }
+        }
+
+        Integer[] trimmedExactArray = trimArray(exactResults, toReturn);
+
+        return trimmedExactArray;
+    }
+
+
+
+    private Integer countDuplicates(Integer numberToCheck, Integer[] intArray) {
+
+        Integer count = 0;
+
+        for (int number : intArray) {
+
+            if(numberToCheck == number) {
+               count++;
+            }
+        }
+        return count;
+    }
+
+    private Integer[] trimArray(Integer[] results, int sizeOfArray) {
+        Integer[] newArray = new Integer[sizeOfArray];
+        int nextIndex = 0;
+        for (int i = 0; i < results.length; i++) {
+            if(results[i] != null) {
+                newArray[nextIndex] = results[i];
+                nextIndex++;
+            }
+        }
+        return newArray;
+    }
 }
+
+
+
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,64 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+
+        String[] results = new String[array.length];
+        int toReturn = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(countDuplicates(array[i], array) < maxNumberOfDuplications) {
+                results[i] = array[i];
+                toReturn++;
+            }
+        }
+
+        String[] trimmedArray = trimArray(results, toReturn);
+
+        return trimmedArray;
+    }
+
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        String[] exactResults = new String[array.length];
+        int toReturn = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(countDuplicates(array[i], array) != exactNumberOfDuplications) {
+                exactResults[i] = array[i];
+                toReturn++;
+            }
+        }
+
+        String[] trimmedExactArray = trimArray(exactResults, toReturn);
+
+        return trimmedExactArray;
+    }
+
+    private Integer countDuplicates(String stringToCheck, String[] intArray) {
+
+        Integer count = 0;
+
+        for (String string : intArray) {
+
+            if(stringToCheck == string) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private String[] trimArray(String[] results, int sizeOfArray) {
+        String[] newArray = new String[sizeOfArray];
+        int nextIndex = 0;
+        for (int i = 0; i < results.length; i++) {
+            if(results[i] != null) {
+                newArray[nextIndex] = results[i];
+                nextIndex++;
+            }
+        }
+        return newArray;
+    }
+
 }
